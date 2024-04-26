2d4d036b12d6e59361ed83207efdf7b4a8768364
Iagao <iagofmoraes@hotmail.com>
2024-04-26 00:41:31 -0300

    Inicio do prj

---
+++ CarrosAPI/Controllers/CarroController.cs
@@ -0,0 +1,34 @@
+﻿using CarrosAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net.NetworkInformation;
+
+namespace CarrosAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CarroController : ControllerBase
+{
+    private static List<Carro> carros = new List<Carro>();
+    private static int id = 0;
+    [HttpPost]
+    public void AdicionaCarro([FromBody] Carro carro)
+    {
+        carro.Id = id++;
+        carros.Add(carro);
+        Console.WriteLine(carro.Marca);
+        Console.WriteLine(carro.Modelo);
+    }
+    [HttpGet]
+    public IEnumerable<Carro> RecuperarCarros()
+    {
+        return carros;
+    }
+
+    [HttpGet]
+    public Carro? RecuperaCarroPorId(int id)
+    {
+        return carros.FirstOrDefault(carro => carro.Id == id);
+    }
+}
+
---
+++ CarrosAPI/Models/Carro.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarrosAPI.Models;
+
+public class Carro
+{
+    [Required]
+    public int Id { get; internal set; }
+    [Required(ErrorMessage = "A Marca do veiculo é obrigatória.")]
+    public string Marca { get; set; }
+    [Required(ErrorMessage = "O Modelo do veiculo é obrigatório.")]
+    public string Modelo { get; set; }
+    [Required(ErrorMessage = "O Ano do veiculo é obrigatório.")]
+    // Erro que não conheço [MaxLength(4, ErrorMessage = "O Ano digitado é invalido.")]
+    public int Ano { get; set; }
+    //[MaxLength(200, ErrorMessage ="A Especificação não pode ter mais de 200 caracteres.")]
+    public string Especificacoes { get; set; }
+    [Required(ErrorMessage = "A Classe do veiculo é obrigatória.")]
+    public string Classe { get; set; }
+    
+}
